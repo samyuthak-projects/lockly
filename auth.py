1b76a0de7476293e1244317f99fefc5dda866749
Samyutha Katikareddy <samyutha.katikareddy@gmail.com>
2026-05-18 16:36:48 +0100

    Build auth user interface

--- auth.py
+++ auth.py
@@ -2,7 +2,22 @@ import customtkinter as ctk
 
 class AuthFrame(ctk.CTkFrame):
     def __init__(self, master):
-        super().__init__(master)
+        super().__init__(master, fg_color="#ffd0eb")
         self.master = master
-        self.title = ctk.CTkLabel(self, text="Welcome to Lockly", font=ctk.CTkFont(size=30, weight="bold"))
-        self.title.pack(pady=40)        
+        self.title = ctk.CTkLabel(self, text="Welcome to Lockly", font=ctk.CTkFont("Helvetica", size=30, weight="bold"), text_color=("#FF0077"))
+        self.title.pack(pady=40)
+
+        self.username_entry = ctk.CTkEntry(self, placeholder_text="Username", width=250)
+        self.username_entry.pack(pady=10)
+
+        self.password_entry = ctk.CTkEntry(self, placeholder_text="Password", width=250, show="*")
+        self.password_entry.pack(pady=10)
+
+        self.login_button = ctk.CTkButton(self, text="Login",bg_color="#4CAF50", fg_color="#4CAF50", hover_color="#45a049")
+        self.login_button.pack(pady=10)
+
+        self.register_button = ctk.CTkButton(self, text="Register",bg_color="#2196F3", fg_color="#2196F3", hover_color="#1976D2")
+        self.register_button.pack(pady=10)
+
+        self.message_label = ctk.CTkLabel(self, text="", font=ctk.CTkFont(size=12), text_color="red")
+        self.message_label.pack(pady=20)
